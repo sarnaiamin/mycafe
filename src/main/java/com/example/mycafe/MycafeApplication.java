51c36ddd416cd3fdc322cd30d3383f6f3b5d6d8d
chinzorigts <chinzorigts@gmail.com>
2024-03-19 16:18:04 +0900

    Init project
    - Init project.

---
+++ src/main/java/com/example/mycafe/MycafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mycafe/MycafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
